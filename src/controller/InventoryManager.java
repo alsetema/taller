4c2cca5378a7dd82c40338d8aaa3eec9c7e14e40
Albert Tellez <alsetema@gmail.com>
2019-04-29 23:31:01 +0200

    Revert "added readfile and basic printinventory"
    This reverts commit 97610befae569b221346744f5b287d329b5dadcd.

--- src/controller/InventoryManager.java
+++ src/controller/InventoryManager.java
@@ -3,7 +3,6 @@ package controller;
 import model.Bar;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,42 +12,24 @@ public class InventoryManager {
 
 
     public InventoryManager() {
-        File inventoryFile = new File("src/data/inventoryData.csv");
+        File inventoryFile = new File("/src/data/inventoryData.csv")
 
 
         if(inventoryFile.exists() && inventoryFile.canRead()) {
-            loadInventory(inventoryFile.getPath());
-
+            loadInventory(inventoryFile);
         } else {
             errMSG = "El archivo de inventario no existe o no hay acceso de lectura";
         }
 
     }
 
-    private void loadInventory(String filePath) {
+    private void loadInventory(File inventoryFile) {
         //todo, implement the method that will read the inventory file
         //and load it to the inventory arraylist
-        try {
-            String fullDatabase = fileToString(filePath);
-            String[] lineArray = fullDatabase.split("\n");
-            for(int counter = 0; counter < lineArray.length; counter++) {
-                String[] innerAttributes = lineArray[counter].split("\\|");
-                //assuming the current data structure in the file
-                String barName = innerAttributes[0];
-                String barSeries = innerAttributes[1];
-                double barLength = Double.parseDouble(innerAttributes[2]);
-                double barThickness = Double.parseDouble(innerAttributes[3]);
-                String barColour = innerAttributes[4];
-
-                Bar tempBar = new Bar(barColour,barSeries,barLength,barThickness,barName);
-                inventory.add(tempBar);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        inventory.add(new Bar("negro", "2300",640, 6, "marco superior"));
 
     }
-//remember to make this private
+
     private String fileToString(String filePath) throws IOException {
         BufferedReader reader = new BufferedReader((new FileReader(filePath)));
         StringBuilder sb = new StringBuilder();
@@ -60,21 +41,11 @@ public class InventoryManager {
         }
 
         reader.close();
-        return sb.toString();
+
     }
 
     public void clearError() {
         this.errMSG = null;
-    }
-
-    public String getErrMSG() { //this system will return the error message and clear it from the memory
-        String outMessage = this.errMSG;
-        clearError();
-        return outMessage;
-    }
-
-    public ArrayList<Bar> getInventory() {
-        return this.inventory;
     }
 
 
